c601af4aa7ab8cbc9e740b14f0a82d20bebd3b7b
den <denlog1977@gmail.com>
2019-11-14 00:24:51 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a411_listview/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.a411_listview;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ListView list = findViewById(R.id.list411);
+
+        List<Map<String, String>> content = prepareContent();
+
+        SimpleAdapter listContentAdapter = createAdapter(content);
+
+        list.setAdapter(listContentAdapter);
+    }
+
+    @NonNull
+    private SimpleAdapter createAdapter(List<Map<String, String>>  content) {
+        return new SimpleAdapter(this, content, android.R.layout.simple_list_item_1, new String[]{"text","length"}, new int[]{R.id.textView, R.id.textView2});
+    }
+
+    @NonNull
+    private List<Map<String, String>> prepareContent() {
+        List<Map<String, String>> contentList = new ArrayList<>();
+        String[] arrayContent = getString(R.string.large_text).split("\n\n");
+        Map<String, String> mapForList;
+        for (int i = 0 ; i < arrayContent.length ; i++){
+            mapForList = new HashMap<>();
+            mapForList.put("text",arrayContent[i]);
+            mapForList.put("length",Integer.toString(arrayContent[i].length()));
+            contentList.add(mapForList);
+        };
+        return contentList;
+    }
+}
